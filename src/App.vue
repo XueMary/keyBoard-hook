0a52613efa81556304531cac51dddf0e668073c2
jihong <632565970@qq.com>
2019-02-20 12:03:43 +0800

    标准resize触发

---
+++ src/App.vue
@@ -0,0 +1,169 @@
+<template>
+  <div id="app">
+    <input type="text">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+class keyboardHook {
+  constructor() {
+    this.winHeight = window.innerHeight;
+    this.tagNames = ["input", "textarea"];
+    this.types = ["text", "password", "number"];
+    this.keyboardFocus = new Event("keyboardFocus");
+    this.keyboardBlur = new Event("keyboardBlur");
+
+    this.isShowKeyBoard = false;
+    this.isResize = false;
+    this.orientation = false;
+    this.init();
+  }
+
+  init() {
+    this.initFocus();
+    this.initBlur();
+    this.initOrientation();
+    this.initClickFocus()
+  }
+
+  initOrientation() {
+    let _this = this;
+    window.addEventListener("orientationchange", function() {
+      _this.orientation = true;
+    });
+  }
+
+  initFocus() {
+    const _this = this;
+    this.resizeF = this.resizeFn(this, this.resizeFocus)
+    window.addEventListener("resize", _this.resizeF);
+  }
+
+  initBlur() {
+    const _this = this;
+    this.resizeB = this.resizeFn(this, this.resizeBlur)
+    window.addEventListener("resize", this.resizeB);
+  }
+
+  initClickFocus () {
+    let _this = this
+    let clickFn = function(e) {
+      let isInput = _this.isInput(e);
+      if (!isInput) return;
+
+      let timeOuts = null;
+      clearTimeout(timeOuts);
+      timeOuts = setTimeout(function() {
+        window.removeEventListener("click", clickFn);
+        if (!_this.isResize) {
+          clickFn = function() {
+            let isInput = _this.isInput(e);
+            if (!isInput) return;
+            _this.clickFocus();
+          };
+          _this.clickFocus();
+          window.addEventListener("click", clickFn);
+          window.removeEventListener("resize", _this.resizeF);
+          window.removeEventListener("resize", _this.resizeB);
+        }
+      }, 500);
+    };
+    window.addEventListener("click", clickFn);
+  }
+
+  resizeF() {}
+  resizeB() {}
+  resizeFn(_this, fn) {
+    return function() {
+      if (_this.orientation) {
+        _this.orientation = false;
+        return;
+      }
+      fn.call(_this);
+    };
+  }
+
+  isInput(e) {
+    function isThunks(arrs) {
+      return function(arr) {
+        arr = arr.toLowerCase();
+        if (arrs.indexOf(arr) !== -1) {
+          return true;
+        }
+        return false;
+      };
+    }
+    const isTag = isThunks(this.tagNames);
+    const isType = isThunks(this.types);
+
+    const event = e || window.event;
+    const target = event.target;
+
+    const tagName = target.tagName.toLowerCase();
+    const type = target.type;
+
+    if (!isTag(tagName)) {
+      return false;
+    } else if (tagName === "input" && !isType(type)) {
+      return false;
+    }
+    return true;
+  }
+
+  clickFocus() {
+    
+  }
+
+  resizeFocus() {
+    let curHeight = window.innerHeight;
+    if (Math.abs(curHeight - this.winHeight) > 80) {
+      if (!this.isShowKeyBoard) {
+        this.isShowKeyBoard = true;
+        this.isResize = true;
+        window.dispatchEvent(this.keyboardFocus);
+      }
+    }
+  }
+  resizeBlur() {
+    let curHeight = window.innerHeight;
+    if (Math.abs(curHeight - this.winHeight) < 80) {
+      if (this.isShowKeyBoard) {
+        this.isShowKeyBoard = false;
+        this.isResize = true;
+        window.dispatchEvent(this.keyboardBlur);
+      }
+    }
+  }
+}
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    new keyboardHook();
+    window.addEventListener("keyboardFocus", () => {
+      alert("focus");
+    });
+    window.addEventListener("keyboardBlur", () => {
+      alert("blur");
+    });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
